fe0a7c51440fbf44f09304fc26a3a75758812743
xiaomile <15622388695@163.com>
2024-01-11 09:02:47 +0800

    Update and rename hf_model_download.py to model_download.py

--- hf_model_download.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# 设置环境变量
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-# 下载模型
-os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /data/model/sentence-transformer')
-
