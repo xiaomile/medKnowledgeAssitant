703905807c60cdc46c6b603f615535232a9d2f97
xiaomile <15622388695@163.com>
2024-01-11 09:03:37 +0800

    Create nltk_download.sh

---
+++ nltk_download.sh
@@ -0,0 +1,8 @@
+cd /root
+git clone https://gitee.com/yzy0612/nltk_data.git  --branch gh-pages
+cd nltk_data
+mv packages/*  ./
+cd tokenizers
+unzip punkt.zip
+cd ../taggers
+unzip averaged_perceptron_tagger.zip
