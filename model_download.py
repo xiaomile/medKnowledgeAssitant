8b060189abba71638640f840b9c6acbd71d8781b
xiaomile <15622388695@163.com>
2024-01-12 00:17:34 +0800

    Update model_download.py

--- model_download.py
+++ model_download.py
@@ -4,9 +4,9 @@ import os
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 # 下载sentence-transformer模型
-os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /model/sentence-transformer')
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /home/xlab-app-center/model/sentence-transformer')
 
 # 下载InternLM-chat-7b模型
 import torch
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
-model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/model', revision='v1.0.3')
+model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/home/xlab-app-center/model', revision='v1.0.3')
